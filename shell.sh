ea2cbdb8938cabc9afc190d8772251b76b80a233
Casper <42568844+nanaones@users.noreply.github.com>
2021-05-17 02:07:07 +0900

    feat: add config for airflow ui editor

---
+++ shell.sh
@@ -0,0 +1,12 @@
+[code_editor]
+git_enabled = True
+git_cmd = /usr/bin/git
+git_default_args = -c color.ui=true
+git_init_repo = False
+root_directory = /usr/local/airflow/dags
+line_length = 88
+string_normalization = False
+mount_name = data
+mount_path = /usr/local/airflow/data
+mount1_name = logs
+mount1_path = /usr/local/airflow/logs
